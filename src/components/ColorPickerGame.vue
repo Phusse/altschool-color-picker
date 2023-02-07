ec4c73d173a01c22e2cba4d00184ff618aa64c4e
Dubem <dubemmegbo@gmail.com>
2023-02-07 23:40:03 +0100

    class of vue(router)

---
+++ src/components/ColorPickerGame.vue
@@ -0,0 +1,18 @@
+import useColorPickerGame from "../composables/ColorPickerGame";
+<template>
+  <h1>Color Picker Game</h1>
+
+  <div>{{ message }}</div>
+
+  <button v-for="color in colors" :key="color" @click="matchColor(color)">
+    {{ color }}
+  </button>
+</template>
+
+<script setup>
+
+const { message, colors, matchColor } = useColorPickerGame();
+
+</script>
+
+
